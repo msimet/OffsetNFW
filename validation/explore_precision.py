39daee76087aabe3464d8da35300f01d88defb02
Melanie Simet <melanie.simet@gmail.com>
2016-07-20 12:21:11 +0100

    Placeholder validation scripts

---
+++ validation/explore_precision.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    # Make a 10^14 c=4 halo, check radial binning
+    # Make a 10^14 c=4 halo, check miscentering binning    
+    # Make a 10^14 c=2 halo, check radial binning
+    # Make a 10^14 c=2 halo, check miscentering binning        
+    # Make a 10^14 c=6 halo, check radial binning
+    # Make a 10^14 c=6 halo, check miscentering binning            
+    # Make a 10^13 c=4 halo, check radial binning
+    # Make a 10^13 c=4 halo, check miscentering binning    
+    # Make a 10^13 c=2 halo, check radial binning
+    # Make a 10^13 c=2 halo, check miscentering binning        
+    # Make a 10^13 c=6 halo, check radial binning
+    # Make a 10^13 c=6 halo, check miscentering binning            
+    # Make a 10^15 c=4 halo, check radial binning
+    # Make a 10^15 c=4 halo, check miscentering binning    
+    # Make a 10^15 c=2 halo, check radial binning
+    # Make a 10^15 c=2 halo, check miscentering binning        
+    # Make a 10^15 c=6 halo, check radial binning
+    # Make a 10^15 c=6 halo, check miscentering binning            
---
+++ validation/make_plots.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    # Plot each kind of thing, interpolated and theoretical, plus the relevant thing from two other packages
+
+    # Plot miscentering vs cluster-lensing miscentering
+    
+    # Plot single miscentering vs Rayleigh miscentering
+    
+    # Show range of parameters for miscentering for each thing
+    
+    # Show individual radial slices for miscentering for each thing
+    
+    
